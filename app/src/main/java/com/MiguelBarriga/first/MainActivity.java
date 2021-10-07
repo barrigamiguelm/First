ff6b7706a51bb65b84b5ad28052f1e0b4fb524e2
am9DAM2 <am9DAM2@informatica.iesjc>
2021-10-07 08:42:29 +0200

    Initial commit

---
+++ app/src/main/java/com/MiguelBarriga/first/MainActivity.java
@@ -0,0 +1,17 @@
+package com.MiguelBarriga.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ *
+ * @author barrigamiguelm
+ *
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+
+}
